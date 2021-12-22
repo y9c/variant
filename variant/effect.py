e550459608749838d6dcabd11e30721ac78e10ce
Chang Ye <yech1990@gmail.com>
2021-12-22 11:24:13 -0600

    add dist to splice

--- variant/effect.py
+++ variant/effect.py
@@ -105,9 +105,30 @@ def parse_eff(eff, pos):
     pad = 10
     pos = int(pos)
     mut_type = type(eff).__name__
+    d2s = []
+    for i, exon in enumerate(eff.transcript.exons):
+        if i == 0:
+            if exon.strand == "+":
+                d2s.append(pos - exon.end)
+            else:
+                d2s.append(exon.start - pos)
+        elif i == len(eff.transcript.exons) - 1:
+            if exon.strand == "+":
+                d2s.append(pos - exon.start)
+            else:
+                d2s.append(exon.end - pos)
+        else:
+            if exon.strand == "+":
+                d2s.append(pos - exon.start)
+                d2s.append(pos - exon.end)
+            else:
+                d2s.append(exon.start - pos)
+                d2s.append(exon.end - pos)
+    # minus distance to splice site
+    distance2splice = sorted(d2s, key=lambda x: abs(x))[0]
     transcript_id = None if mut_type == "Intergenic" else eff.transcript_id
 
-    non_exon_recored = [mut_type, eff.gene_name, transcript_id] + [None] * 6
+    non_exon_recored = [mut_type, eff.gene_name, transcript_id] + [None] * 7
     if mut_type in ["Intergenic", "Intronic", "SpliceDonor"]:
         return non_exon_recored
     try:
@@ -169,6 +190,7 @@ def parse_eff(eff, pos):
         codon_ref,
         aa_pos,
         aa_ref,
+        distance2splice,
     ]
 
 
@@ -236,6 +258,7 @@ def run():
             "codon_ref",
             "aa_pos",
             "aa_ref",
+            "distance2splice",
         ]
         print("#" + "\t".join(input_header + annot_header), file=output_f)
         for l in f:
